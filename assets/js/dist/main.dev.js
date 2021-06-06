3310f9724dc846b0a578ed8865bfbfa9f524c675
Anton Samoilenko <antonplane@gmail.com>
2021-06-07 00:49:37 +0300

    add map

--- assets/js/dist/main.dev.js
+++ assets/js/dist/main.dev.js
@@ -22,19 +22,42 @@ $(function () {
   $('#nextSlide').on('click', function () {
     horizontalSlider.goToNextSlide();
   });
-  /* $('#lightgallery').lightGallery({
-      plugins: [lgZoom, lgThumbnail],
-      conter: false,
-      download: false,
-      speed: 500,
-      animateThumb: false,
-      zoomFromOrigin: false,
-      allowMediaOverlap: true,
-      toggleThumb: true,
-      
-    }); */
+  /*  $('#lightgallery').lightGallery({
+       plugins: [lgZoom, lgThumbnail],
+       conter: false,
+       download: false,
+       speed: 500,
+       animateThumb: false,
+       zoomFromOrigin: false,
+       allowMediaOverlap: true,
+       toggleThumb: true,
+       
+     }); */
 });
-lightGallery(document.getElementById('lightgallery'), {
-  plugins: [lgZoom, lgThumbnail],
-  speed: 500
-});
+/* lightGallery(document.getElementById('lightgallery'), {
+    plugins: [lgZoom, lgThumbnail],
+    speed: 500,
+    
+}); */
+
+function initMap() {
+  map = L.map('map').setView([49.091678, 33.451642], 13);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+  var LeafIcon = L.Icon.extend({
+    options: {
+      iconSize: [106, 106],
+      iconAnchor: [106, 106],
+      popupAnchor: [-22, -30]
+    }
+  });
+  var marker = L.marker([49.091678, 33.451642], {
+    icon: new LeafIcon({
+      iconUrl: 'assets/images/leaf-round.png'
+    })
+  }).addTo(map).bindPopup("\n        \n        <p>\u0414\u0430\u0430\u0430, \u043F\u0440\u0438\u043A\u043E\u043B\u044C\u043D\u044B\u0439 \u044D\u043A\u0437\u0430\u043C\u0435\u043D)</p>\n    ");
+  L.Control.remove();
+}
+
+initMap();
--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ $(function(){
         horizontalSlider.goToNextSlide()
     });
 
-    /* $('#lightgallery').lightGallery({
+   /*  $('#lightgallery').lightGallery({
         plugins: [lgZoom, lgThumbnail],
         conter: false,
         download: false,
@@ -39,8 +39,32 @@ $(function(){
 
 });
 
-lightGallery(document.getElementById('lightgallery'), {
+/* lightGallery(document.getElementById('lightgallery'), {
     plugins: [lgZoom, lgThumbnail],
     speed: 500,
     
-});
+}); */
+
+function initMap() {
+    map = L.map('map').setView([49.091678, 33.451642], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    let LeafIcon = L.Icon.extend({
+        options: {
+            iconSize: [106, 106],
+            iconAnchor: [106, 106],
+            popupAnchor: [-22, -30]
+        }
+    });
+
+    let marker = L.marker([49.091678, 33.451642], {icon: new LeafIcon({ iconUrl: 'assets/images/leaf-round.png' })}).addTo(map)
+        .bindPopup(`
+        
+        <p>Дааа, прикольный экзамен)</p>
+    `);
+    L.Control.remove();
+}
+
+initMap();
